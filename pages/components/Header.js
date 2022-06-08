e447cb63d3a8ec9201ec0dd5f134c71360f9e8d7
BatoolBtoush <batoolbtoush98@gmail.com>
2022-06-08 18:00:57 +0300

    almost done

---
+++ pages/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="px-8 py-6 bg-[#3BB99F]">
+        <h1 className="text-4xl font-bold text-black">Cookie Stand Admin</h1>
+    </header>
+  )
+}
+
+export default Header
